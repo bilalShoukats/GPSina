d198d6aa8856703dbf19080f38fa5089894e1cea
jawad shah <informjawad@gmail.com>
2020-03-04 19:32:40 +0500

    change address server for demo

--- app/components/WebSocket/index.js
+++ app/components/WebSocket/index.js
@@ -36,7 +36,7 @@ export default class SocketComponent {
     connectSocketServer = async (hash, deviceIds) => {
         console.log("connect with socket");
         // this.socket = new WebSocket('ws://13.228.129.207:8091');
-        this.socket = new WebSocket('ws://localhost:8091');
+        this.socket = new WebSocket('ws://13.233.162.122:8091');
         this.token = hash;
         this.deviceIDs = deviceIds;
 
