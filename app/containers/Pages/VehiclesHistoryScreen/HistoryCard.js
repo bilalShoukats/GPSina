e385c9442c29da59d50129c2bf905393cc61a676
jawad shah <informjawad@gmail.com>
2020-03-17 12:25:43 +0500

    fix date time on view vehicles history

--- app/containers/Pages/VehiclesHistoryScreen/HistoryCard.js
+++ app/containers/Pages/VehiclesHistoryScreen/HistoryCard.js
@@ -8,52 +8,59 @@ import { Grid } from '@material-ui/core';
 import Card from '../../../components/Card';
 import './style.scss';
 
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-const HistoryCard = (props) => (
-  <Card>
-    <Grid className="responsiveTable">
-      <Table className="postTable" aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Registration Number</TableCell>
-            <TableCell>Company Email</TableCell>
-            <TableCell>Driver Name</TableCell>
-            <TableCell>Assigned At</TableCell>
-            <TableCell>Unassigned At</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>
-              <strong className="uThum">
-                {props.item.registrationNo}
-              </strong>
-            </TableCell>
-            <TableCell>
-              <strong className="uThum">
-                {props.item.companyEmail}
-              </strong>
-            </TableCell>
-            <TableCell>
-              <strong className="uThum">
-                {props.item.driverID}
-              </strong>
-            </TableCell>
-            <TableCell>
-              <strong className="uThum">
-                { Date(props.item.assignedAt).toString() }
-              </strong>
-            </TableCell>
-            <TableCell>
-              <strong className="uThum">
-                { Date(props.item.unAssignedAt).toString() }
-              </strong>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </Grid>
-  </Card>
-)
+const HistoryCard = (props) => {
+  let date = new Date(props.item.assignedAt / 1000000);
+  let unassignDate = new Date(props.item.unAssignedAt / 1000000);
+  let assignedAtDate = days[date.getDay()] + " " + months[date.getMonth()] + " " +date.getDate()+ " "+date.getFullYear()+ " "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+  let unassignedDate = days[unassignDate.getDay()] + " " + months[unassignDate.getMonth()] + " " +unassignDate.getDate()+ " "+unassignDate.getFullYear()+ " "+unassignDate.getHours()+":"+unassignDate.getMinutes()+":"+unassignDate.getSeconds();
+  return (
+    <Card>
+      <Grid className="responsiveTable">
+        <Table className="postTable" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Registration Number</TableCell>
+              <TableCell>Company Email</TableCell>
+              <TableCell>Driver Name</TableCell>
+              <TableCell>Assigned At</TableCell>
+              <TableCell>Unassigned At</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <strong className="uThum">
+                  {props.item.registrationNo}
+                </strong>
+              </TableCell>
+              <TableCell>
+                <strong className="uThum">
+                  {props.item.companyEmail}
+                </strong>
+              </TableCell>
+              <TableCell>
+                <strong className="uThum">
+                  {props.item.driverID}
+                </strong>
+              </TableCell>
+              <TableCell>
+                <strong className="uThum">
+                  {assignedAtDate}
+                </strong>
+              </TableCell>
+              <TableCell>
+                <strong className="uThum">
+                {unassignedDate}
+                </strong>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Grid>
+    </Card>
+  )
+}
 export default HistoryCard;
-
