fd5ce9e4434377da0354bd61ca4a53d14131797b
mac <muhammad.ayaz@azure-i.com>
2021-10-16 16:06:08 +0500

    added formik with yup on add device

--- app/containers/AddDevicePage/index.js
+++ app/containers/AddDevicePage/index.js
@@ -1,60 +1,77 @@
-import React, { useEffect, useState } from 'react';
+import * as yup from 'yup';
+import { compose } from 'redux';
+import messages from './messages';
+import { useFormik } from 'formik';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { FormattedMessage, injectIntl } from 'react-intl';
-import { createStructuredSelector } from 'reselect';
-import { compose } from 'redux';
 import { Helmet } from 'react-helmet';
-import messages from './messages';
-import Header from '../../components/Header';
-import ApiManager from '../../ApiManager/ApiManager';
-import APIURLS from '../../ApiManager/apiUrl';
 import { useStyles } from './styles.js';
-import { Button, Grid, Input, Typography, Radio } from '@material-ui/core';
+import Header from '../../components/Header';
+import APIURLS from '../../ApiManager/apiUrl';
+import React, { useEffect, useState } from 'react';
+import { createStructuredSelector } from 'reselect';
+import ApiManager from '../../ApiManager/ApiManager';
 import UserAvatar from '../../components/UserAvatar';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import satellite from '../../../assets/images/icons/satellite.svg';
+import {
+    Button,
+    Grid,
+    Input,
+    TextField,
+    Typography,
+    Radio,
+} from '@material-ui/core';
 
 export function AddDevicePage(props) {
     const classes = useStyles(props);
-    const [softwareVersion, setSoftwareVersion] = useState('');
-    const [trakerNumber, setTrakerNumber] = useState('');
+    const api = ApiManager.getInstance();
     const [activeDevice, setActiveDevice] = useState('');
-    const [errors, setErrors] = useState({
-        softwareVersion: '',
-        trakerNumber: '',
+
+    const validationSchema = yup.object({
+        softwareVersion: yup
+            .string('Enter software version')
+            .matches(/^\d{1,2}\.\d{1,2}\.\d{1,2}$/, {
+                excludeEmptyString: true,
+                message: 'please enter valid software version',
+            })
+            .min(2, 'software version should be of minimum 2 characters length')
+            .required('software version is required'),
+        trakerNumber: yup
+            .string('Enter traker number')
+            .min(2, 'traker number should be of minimum 2 characters length')
+            .required('traker number is required'),
     });
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (validateForm(errors)) {
-            if (softwareVersion == '' || trakerNumber < 9999) {
-                console.log('Required all Feilds');
-            } else {
-                const body = {
-                    softwareVer: softwareVersion,
-                    deviceID: trakerNumber,
-                    isDumy: true,
-                };
-                const api = ApiManager.getInstance();
-                api.send('POST', APIURLS.addDevice, body)
-                    .then(res => {
-                        if (res.data.code === 1008) {
-                            props.history.goBack();
-                        } else {
-                            console.log('Bad Body ADD DEVICE');
-                        }
-                    })
-                    .catch(error => {
-                        console.log('Error', error);
-                    });
-            }
-        }
-    };
+    const formik = useFormik({
+        initialValues: {
+            trakerNumber: '',
+            softwareVersion: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: values => {
+            api.send('POST', APIURLS.addDevice, {
+                softwareVersion: values.softwareVersion,
+                trakerNumber: values.trakerNumber,
+            })
+                .then(res => {
+                    if (res.data.code === 1008) {
+                        props.history.goBack();
+                    } else {
+                        console.log('Bad Body ADD DEVICE');
+                    }
+                })
+                .catch(error => {
+                    console.log('Error', error);
+                });
+        },
+    });
+
     const handleChange = e => {
         e.preventDefault();
         const { name, value } = e.target;
         let error = errors;
-        const validVersionRegex = RegExp(/^\d{1,2}\.\d{1,2}\.\d{1,2}$/);
+        const validVersionRegex = RegExp();
         switch (name) {
             case 'softwareVersion':
                 error.softwareVersion = validVersionRegex.test(value)
@@ -81,8 +98,6 @@ export function AddDevicePage(props) {
         Object.values(errors).forEach(val => val.length > 0 && (valid = false));
         return valid;
     };
-
-    useEffect(() => {}, []);
 
     return (
         <Grid>
@@ -111,89 +126,111 @@ export function AddDevicePage(props) {
                         <Typography variant="h5" className={classes.title}>
                             <FormattedMessage {...messages.deviceInformation} />
                         </Typography>
-                        <Grid item>
-                            <Typography
-                                variant="body1"
-                                className={classes.label}
-                            >
-                                <FormattedMessage
-                                    {...messages.softwareVersion}
+                        <form onSubmit={formik.handleSubmit} id="myForm">
+                            <Grid item>
+                                <Typography
+                                    variant="body1"
+                                    className={classes.label}
+                                >
+                                    <FormattedMessage
+                                        {...messages.softwareVersion}
+                                    />
+                                </Typography>
+                                <TextField
+                                    id="softwareVersion"
+                                    name="softwareVersion"
+                                    className={classes.textInput}
+                                    value={formik.values.softwareVersion}
+                                    InputProps={{
+                                        classes: { input: classes.textColor },
+                                    }}
+                                    onChange={formik.handleChange}
+                                    placeholder={props.intl.formatMessage({
+                                        ...messages.enterSoftware,
+                                    })}
+                                    error={
+                                        formik.touched.softwareVersion &&
+                                        Boolean(formik.errors.softwareVersion)
+                                    }
+                                    helperText={
+                                        formik.touched.softwareVersion &&
+                                        formik.errors.softwareVersion
+                                    }
                                 />
-                            </Typography>
-                            <Input
-                                className={classes.textInput}
-                                value={softwareVersion}
-                                name="softwareVersion"
-                                placeholder={props.intl.formatMessage({
-                                    ...messages.enterSoftware,
-                                })}
-                                onChange={handleChange}
-                                disableUnderline
-                            />
-                            {errors.softwareVersion.length > 0 && (
-                                <span className={classes.error}>
-                                    {errors.softwareVersion}
-                                </span>
-                            )}
-                        </Grid>
-                        <Grid item>
-                            <Typography
-                                variant="body1"
-                                className={classes.label}
-                            >
-                                <FormattedMessage {...messages.trakerNumber} />
-                            </Typography>
-                            <Input
-                                className={classes.textInput}
-                                value={trakerNumber}
-                                name="trakerNumber"
-                                placeholder={props.intl.formatMessage({
-                                    ...messages.enterTraker,
-                                })}
-                                type="number"
-                                onChange={handleChange}
-                                disableUnderline
-                            />
-                            {errors.trakerNumber.length > 0 && (
-                                <span className={classes.error}>
-                                    {errors.trakerNumber}
-                                </span>
-                            )}
-                        </Grid>
+                            </Grid>
+                            <Grid item>
+                                <Typography
+                                    variant="body1"
+                                    className={classes.label}
+                                >
+                                    <FormattedMessage
+                                        {...messages.trakerNumber}
+                                    />
+                                </Typography>
+                                <TextField
+                                    type="number"
+                                    id="trakerNumber"
+                                    name="trakerNumber"
+                                    className={classes.textInput}
+                                    value={formik.values.trakerNumber}
+                                    InputProps={{
+                                        classes: { input: classes.textColor },
+                                    }}
+                                    placeholder={props.intl.formatMessage({
+                                        ...messages.enterTraker,
+                                    })}
+                                    onChange={formik.handleChange}
+                                    error={
+                                        formik.touched.trakerNumber &&
+                                        Boolean(formik.errors.trakerNumber)
+                                    }
+                                    helperText={
+                                        formik.touched.trakerNumber &&
+                                        formik.errors.trakerNumber
+                                    }
+                                />
+                                {/* {formik.touched.trakerNumber &&
+                                    Boolean(formik.errors.trakerNumber) && (
+                                        <span className={classes.error}>
+                                            {errors.trakerNumber}
+                                        </span>
+                                    )} */}
+                            </Grid>
 
-                        <Grid item className={classes.radioSelection}>
-                            <Grid
-                                container
-                                direction="row"
-                                justifyContent="flex-start"
-                                alignItems="center"
-                            >
-                                <Grid item xs={6} sm={4} md={3}>
-                                    <Grid container alignItems="center">
-                                        <Radio
-                                            checked={activeDevice}
-                                            onChange={() => {
-                                                if (activeDevice === '')
-                                                    setActiveDevice(
-                                                        'activeDevice',
-                                                    );
-                                                else setActiveDevice('');
-                                            }}
-                                            value={activeDevice}
-                                            style={{ color: '#28ACEA' }}
-                                        />
-                                        <Typography
-                                            variant="body1"
-                                            className={classes.label}
-                                        >
-                                            <FormattedMessage
-                                                {...messages.activeDevice}
+                            <Grid item className={classes.radioSelection}>
+                                <Grid
+                                    container
+                                    direction="row"
+                                    justifyContent="flex-start"
+                                    alignItems="center"
+                                >
+                                    <Grid item xs={6} sm={4} md={3}>
+                                        <Grid container alignItems="center">
+                                            <Radio
+                                                checked={activeDevice}
+                                                onChange={() => {
+                                                    if (activeDevice === '')
+                                                        setActiveDevice(
+                                                            'activeDevice',
+                                                        );
+                                                    else setActiveDevice('');
+                                                }}
+                                                value={activeDevice}
+                                                style={{ color: '#28ACEA' }}
                                             />
-                                        </Typography>
+                                            <Typography
+                                                variant="body1"
+                                                className={classes.label}
+                                            >
+                                                <FormattedMessage
+                                                    {...messages.activeDevice}
+                                                />
+                                            </Typography>
+                                        </Grid>
                                     </Grid>
                                 </Grid>
                             </Grid>
-                        </Grid>
+                        </form>
                     </Grid>
                     <Grid
                         container
@@ -202,11 +239,13 @@ export function AddDevicePage(props) {
                         className={classes.btnContainer}
                     >
                         <Button
+                            form="myForm"
+                            size="medium"
+                            type="submit"
                             color="primary"
                             variant="contained"
-                            size="medium"
                             className={classes.btnBlue}
-                            onClick={handleSubmit}
+                            //onSubmit={formik.handleSubmit}
                         >
                             <Typography variant="body1">
                                 <FormattedMessage {...messages.submit} />
