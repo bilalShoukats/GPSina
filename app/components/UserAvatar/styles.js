a609bf22ba33648d3e71e76afbb0fff0348e0f05
Syed Jawad Hassan <jawad.hassan@azure-i.com>
2021-11-05 12:19:32 +0500

    pasted jazib zip code

--- app/components/UserAvatar/styles.js
+++ app/components/UserAvatar/styles.js
@@ -1,12 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
-  avatar: {
-    width: '70px',
-    height: '70px',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    avatar: {
+        width: '70px',
+        height: '70px',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 }));
 
 export { useStyles };
